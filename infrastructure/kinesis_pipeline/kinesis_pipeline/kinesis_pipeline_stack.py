3058f28db5c1990a519772798f5ec53965993626
sean_f <44120999+MyNameIsTakenOMG@users.noreply.github.com>
2025-06-11 19:06:21 -0400

    Revert "finish automating ETL processing of Kinesis_pipeline"
    This reverts commit 41b430ee301df3e2225411b37c80183a1e0d25ba.

--- infrastructure/kinesis_pipeline/kinesis_pipeline/kinesis_pipeline_stack.py
+++ infrastructure/kinesis_pipeline/kinesis_pipeline/kinesis_pipeline_stack.py
@@ -1,13 +1,10 @@
 from aws_cdk import (
-    Duration,
+    # Duration,
     RemovalPolicy,
     Stack,
     aws_s3 as s3,
     aws_kinesis as kinesis,
     aws_kinesisfirehose as firehose,
-    aws_s3_notifications as s3n,
-    aws_lambda as _lambda,
-    aws_lambda_python_alpha as lambda_py,
     aws_iam as iam,
 )
 from constructs import Construct
@@ -68,81 +65,5 @@ class KinesisPipelineStack(Stack):
             ),
         )
 
-        # # etl handler lambda layer (using the aws pre-built layer for AWS SDK for Pandas)
-        # etl_layer = _lambda.LayerVersion.from_layer_version_arn(
-        #     self,
-        #     "ETLLayer",
-        #     "arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python312:17",
-        # )
-
-        # # Lambda function for auto-ETL processing
-        # etl_fn = _lambda.Function(
-        #     self,
-        #     "ETLFunction",
-        #     runtime=_lambda.Runtime.PYTHON_3_12,
-        #     timeout=Duration.minutes(3),
-        #     handler="etl_handler.lambda_handler",
-        #     code=_lambda.Code.from_asset("functions"),
-        #     memory_size=256,
-        #     layers=[etl_layer],
-        #     environment={
-        #         "RAW_BUCKET": raw_bucket.bucket_name,
-        #         "RAW_PREFIX": "lake/raw/batch/",
-        #         "CLEAN_PREFIX": "lake/clean/batch/",
-        #         "GLUE_DATABASE": "ecommerce_data_lake",
-        #         "GLUE_TABLE": "clean_batch",
-        #     },
-        #     initial_policy=[
-        #         iam.PolicyStatement(
-        #             actions=[
-        #                 "glue:GetTable",
-        #                 "glue:GetTables",
-        #                 "glue:CreateTable",
-        #                 "glue:UpdateTable",
-        #                 "glue:GetDatabase",
-        #                 "s3:DeleteObject",
-        #             ],
-        #             resources=["*"],
-        #         )
-        #     ],
-        # )
-        # # needs docker to bundle the python lambda function with dependencies
-        # etl_fn = lambda_py.PythonFunction(
-        #     self,
-        #     "ETLFunction",
-        #     entry="functions",
-        #     runtime=_lambda.Runtime.PYTHON_3_12,
-        #     index="etl_handler.py",
-        #     timeout=Duration.seconds(30),
-        #     environment={
-        #         "RAW_BUCKET": raw_bucket.bucket_name,
-        #         "RAW_PREFIX": "lake/raw/bathc/",
-        #         "CLEAN_PREFIX": "lake/clean/batch/",
-        #         "GLUE_DATABASE": "ecommerce_data_lake",
-        #         "GLUE_TABLE": "clean_batch",
-        #     },
-        #     initial_policy=[
-        #         iam.PolicyStatement(
-        #             actions=[
-        #                 "glue:GetTable",
-        #                 "glue:GetTables",
-        #                 "glue:CreateTable",
-        #                 "glue:UpdateTable",
-        #                 "glue:GetDatabase",
-        #             ],
-        #             resources=["*"],
-        #         )
-        #     ],
-        # )
-
-        # # NOTE: to use s3 notification with lambda, must increase the lambda memory size and timeout
-        # # Grant the Lambda function permissions to read and write from the raw S3 bucket
-        # raw_bucket.grant_read(etl_fn)
-        # raw_bucket.grant_put(etl_fn)
-
-        # # Configure S3 notifications to trigger the Lambda function
-        # raw_bucket.add_event_notification(
-        #     s3.EventType.OBJECT_CREATED,
-        #     s3n.LambdaDestination(etl_fn),
-        #     s3.NotificationKeyFilter(prefix="lake/raw/batch/", suffix=".csv"),
-        # )
+        self.bucket_name = raw_bucket.bucket_name
+        self.stream_name = stream.stream_name
